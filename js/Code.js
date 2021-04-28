f31658a594ea2c1a4a5434e2b947a7bd856ee4b3
rajjadhav-hash <sj113896@gmail.com>
2021-04-28 20:11:40 +0530

    first commit

---
+++ js/Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("yellow")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white");
+    textSize(15);
+    text("C U T N I F O N",710,160);
+    fill("yellow");
+    text("Hint: Perform a particular task !!",710,180);
+
+    fill("white");
+    textSize(15);
+    text("A T A D A S B A",120,250);
+    fill("yellow");
+    text("Hint: Stores all information",120,275);
+
+
+    
+
+}
